dd51ecfae453ef47b9be6b1203136502bb3a2595
Sphurthi <Sphurthi.rayala@outlook.com>
2020-07-29 23:27:18 -0400

    Added Log-In and Password fields and created validation

--- AskUsFresh/ClientApp/src/app/app.module.ts
+++ AskUsFresh/ClientApp/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -27,6 +29,7 @@ import { LogInComponent } from './log-in/log-in.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+   ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- AskUsFresh/ClientApp/src/app/log-in/log-in.component.ts
+++ AskUsFresh/ClientApp/src/app/log-in/log-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-log-in',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
+  title = 'Please Enter Your Email and Password';
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+  createForm() {
+    this.angForm = this.fb.group({
+      Email: ['', Validators.required],
+      Password: ['', Validators.required]
+    });
+  }
 
-  constructor() { }
+  //createForm2() {
+  //  this.angForm = this.fb.group({
+  //    Password: ['', Validators.required]
+  //  });
+  //}
 
   ngOnInit() {
   }
