a907e7776ac7463352bf1e8c5100a622f0d06bb4
Sphurthi <Sphurthi.rayala@outlook.com>
2020-08-06 15:31:33 -0400

    added address tabl

---
+++ AskUsFresh.Database/Address.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Address1] VARCHAR(120) NOT NULL, 
+    [Address2] VARCHAR(120) NULL, 
+    [Address3] VARCHAR(120) NULL, 
+    [City] VARCHAR(120) NOT NULL,
+    [State] CHAR(2) NOT NULL,
+    [Country] VARCHAR(20) NOT NULL,
+    [ZipCode] VARCHAR(16) NOT NULL,
+)
